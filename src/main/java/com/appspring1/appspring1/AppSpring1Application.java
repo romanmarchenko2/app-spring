3e6f28491ca493a9a29f61603f3bc769f0e30bcd
romanmarchenko22 <pumacore22@gmail.com>
2020-02-03 11:07:41 +0200

    Me First Commit

---
+++ src/main/java/com/appspring1/appspring1/AppSpring1Application.java
@@ -0,0 +1,13 @@
+package com.appspring1.appspring1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppSpring1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AppSpring1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/appspring1/appspring1/AppSpring1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.appspring1.appspring1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppSpring1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
